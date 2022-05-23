f9ed1bb634cb2c2290fee4e37824c76f3e1ae9bb
shyambiradar <biradarshyam1999@gmail.com>
2022-05-23 11:48:10 +0000

    first project

---
+++ SectionTwo.java
@@ -0,0 +1,16 @@
+package InitialName;
+
+public class SectionTwo {
+public static void main(String[] args) {
+	int passengers=5;
+	System.out.println(passengers);
+     passengers+=10;
+     System.out.println(passengers);
+     passengers-=4;
+     System.out.println(passengers);
+     passengers-=7;
+     System.out.println(passengers);
+     System.out.println("remaining passangers are "+passengers);
+     
+}
+}
