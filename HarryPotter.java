cee261328a263b395d81870a3256dbd0baed1df6
shyambiradar <biradarshyam1999@gmail.com>
2022-05-23 11:43:46 +0000

    first project

---
+++ HarryPotter.java
@@ -0,0 +1,20 @@
+package InitialName;
+
+public class HarryPotter {
+public static void main(String[] args) {
+	int points=0;
+	System.out.println("Harry was caught wandering the hall, -50 points for grinffindor");
+	points-=50;
+	System.out.println("Harry was being cheeky in class. -3 point for grinffindor");
+	points-=3;
+	System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for grinffindor");
+	points+=30;
+	System.out.println("Ron won the underground chess match. 100 point for grinffindor");
+	points+=100;
+	System.out.println("Harry defeated the Quirrells. 60 points for grinffindor");
+	points+=60;
+	System.out.println("Total score is "+ points);
+	
+	
+}
+}
